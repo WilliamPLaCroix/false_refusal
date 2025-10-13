3391526672cf2e7f69b3b6ac5b77b105d6cb3fca
WilliamPLaCroix <williamplacroix@gmail.com>
2025-10-13 20:59:52 +0200

    check if doing on cluster is faster (duh)

---
+++ correctness.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import shap
+from tqdm import tqdm
+import torch
+
+def main():
+    xsb_dataset = pd.read_csv('data/XSB.csv', header='infer', usecols=['prompt', 'label', 'focus'])
+    print(f"XSB dataset shape: {xsb_dataset.shape}")
+    xsb_dataset
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using device: {device}")
+    tokenizer = AutoTokenizer.from_pretrained("gpt2", use_fast=True)
+    model = AutoModelForCausalLM.from_pretrained("gpt2").to(device)
+    # set model decoder to true
+    model.config.is_decoder = True
+    # set text-generation params under task_specific_params
+    model.config.task_specific_params["text-generation"] = {
+        "do_sample": True,
+        "max_length": 50,
+        "temperature": 0,
+        "top_k": 5,
+        "no_repeat_ngram_size": 2,
+    }
+    explainer = shap.Explainer(model, tokenizer)
+
+    guesses = []
+    correctness = []
+    shap_values = explainer(xsb_dataset["prompt"])
+
+    for row in tqdm(xsb_dataset.itertuples()):
+        index = row.Index
+        prompt = row.prompt
+        label = row.label
+        focus = row.focus
+        #print(f"Prompt: {prompt}\nLabel: {label}\nFocus: {focus}")
+
+        #print(shap_values)
+        trigger_token_index = np.mean(shap_values.values[index], axis=1).argmax()
+        trigger_token = shap_values[index].data[trigger_token_index]
+        #print(f"Trigger token: {trigger_token}\n")
+        guesses.append(trigger_token.strip())
+        correctness.append(trigger_token in focus)
+
+    print(f"Guesses: {guesses}\nCorrectness: {correctness}")
+
+    xsb_dataset['guesses'] = guesses
+    xsb_dataset['correctness'] = correctness
+
+    # find percent correct
+    percent_correct = sum(correctness) / len(correctness) * 100
+    print(f"Percent correct: {percent_correct:.2f}%")
+
+    # find percent corrent when label is safe
+    safe_rows = xsb_dataset[xsb_dataset['label'] == 'safe']
+    percent_correct_safe = sum(safe_rows['correctness']) / len(safe_rows) * 100
+    print(f"Percent correct when label is safe: {percent_correct_safe:.2f}%")
+    # find percent corrent when label is unsafe
+    unsafe_rows = xsb_dataset[xsb_dataset['label'] == 'unsafe']
+    percent_correct_unsafe = sum(unsafe_rows['correctness']) / len(unsafe_rows) * 100
+    print(f"Percent correct when label is unsafe: {percent_correct_unsafe:.2f}%")
+
+    xsb_dataset.to_csv('data/xsb_with_guesses.csv', index=False)
+
+
+if __name__ == "__main__":
+    !pip install shap
+    main()
